cc5fe4d9646c8b4c05607a0a7c52d18feb909d4f
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-11-21 10:24:33 -0500

    74 Concept Repetition: Working with State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,28 @@
-import '../index';
+import { useState } from "react";
 
 export default function Player({name, symbol}){
+
+    const [isEditing, setIsEditing] = useState(false);
+    
+    let playerName = <span className="player-name">{name}</span>;
+    let buttonName = "Edit";
+
+    function handleEditClick(){                
+        setIsEditing(isEditing => !isEditing);
+    }
+
+    if(isEditing){
+        playerName = <input type="text" required placeholder="Ingresa tu dato"></input>;
+        buttonName = "Save";
+    }
+    
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{buttonName}</button>
         </li>
     );
 }
