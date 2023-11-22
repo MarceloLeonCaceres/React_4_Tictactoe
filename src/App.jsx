1d0ea84fd3f108b4aa9bb14598e3ceb97c49591e
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2023-11-22 13:19:59 -0500

    89 Lifting Computed Values Up

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Log from './components/Log.jsx';
 
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function deriveActivePlayer(gameTurns){
   
   let currentPlayer = 'X';
@@ -20,7 +26,21 @@ function App() {
 
   const [gameTurns, setGameTurns] = useState([]);
   // const[activePlayer, setActivePlayer] = useState('X');
+  // cons[hasWinner, setHasWinner] = useState(false);
+
   const activePlayer = deriveActivePlayer(gameTurns);
+  
+  let gameBoard = initialGameBoard;
+  
+  for(const turn of gameTurns){
+      const {square, player} = turn;
+      const {row, col} = square;
+      gameBoard[row][col] = player;
+  }
+
+  for(const combination of WINNING_COMBINATIONS){
+
+  }
 
   function handleSelectSquare(rowIndex, colIndex){
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -47,7 +67,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive ={ activePlayer==='O' }></Player>
         </ol>
 
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
 
       <Log turns={gameTurns}/>
